4c6658c4c1f70220902680762fa892fed039d886
Longhui Zheng <2411295646@qq.com>
2023-09-08 15:13:07 +0800

    get danmus from single vedio

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+#coding=utf-8
+import requests
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69'
+}
+url = 'https://www.bilibili.com/video/BV1yF411C7ZJ/?spm_id_from=333.337.search-card.all.click&vd_source=884af3d08eeae21ab00e4b40b06e8ca8'
+
+response = requests.get(url, headers=headers)
+html = response.text
+
+cid = re.search('"cid":(.*?),', html).groups()[0]
+print(cid)
+danmu_url = f'https://comment.bilibili.com/{cid}.xml'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+response = requests.get(danmu_url, headers=headers)
+response.encoding = 'utf-8'
+html = response.text
+
+contexts = re.findall('<d p=".*?">(.*?)</d>', html)
+print(contexts)
+with open('./danum.txt', 'w', encoding='utf-8') as f:
+    for context in contexts:
+        f.write(context + '\n')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
